e58801c69a3ac17dc8dc914e6c19ff8c5d15e4e8
hhoburg <hhoburg@gmail.com>
2016-02-24 16:40:45 -0700

    added endGame route

--- routes/endGame.js
+++ routes/endGame.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function endGameHandler (req, res, next) {
-  res.send('you made it!!!');
-
+  // res.send('you made it!!!');
+  knex('games_players').where('game_id', req.body.game_id).del();
+  knex('stories').where('game_id', req.body.game_id).del();
+  knex('games').where('game_id', req.body.game_id).del();
 }
 
 
