0483674339d15511145e3d7aa1d4d11006b523af
hhoburg <hhoburg@gmail.com>
2016-02-24 15:17:59 -0700

    fixed some migration stuff

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
-// module.exports = {
-//
-//   development: {
-//     client: 'pg',
-//     connection: 'postgres://localhost/life'
-//   },
-//
-//   production: {
-//     client: 'pg',
-//     connection: process.env.DATABASE_URL
-//   }
-//
-// };
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/life'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+
+};
