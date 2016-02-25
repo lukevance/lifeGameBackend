f92930018988146be8b224ea67632325d12324c1
hhoburg <hhoburg@gmail.com>
2016-02-25 10:39:30 -0700

    finished endTurn route

--- routes/endTurn.js
+++ routes/endTurn.js
@@ -3,7 +3,8 @@
 var knex = require('../db/knex');
 
 //Function to update player info in games_players table
-function endTurnHandler (req, res, next) {
+function endTurnHandler (req) {
+  //Selecting player from the table by id and game_id
   knex('games_players').where({
     game_id: req.body.game_id,
     player_id: req.body.player_id
@@ -18,9 +19,8 @@ function endTurnHandler (req, res, next) {
     salary: req.body.salary,
     addiction: req.body.addiction
   }).then(function() {
-    console.log('new player values: ' + req.body.money + req.body.position + req.body.occupation + req.body.college + req.body.marriage + req.body.kids + req.body.kids + req.body.chipotle + req.body.salary + req.body.addiction)
-  })
-
+    console.log('new player values: ' + req.body.money + req.body.position + req.body.occupation + req.body.college + req.body.marriage + req.body.kids + req.body.kids + req.body.chipotle + req.body.salary + req.body.addiction);
+  });
 }
 
 
