b0f2bb9235c9d82cded5576e688f8cc3da298590
Matt Mobley <mattsmobley@gmail.com>
2016-02-25 10:21:53 -0700

    made some seed data for our database

--- routes/newPlayer.js
+++ routes/newPlayer.js
@@ -1,14 +1,22 @@
 'use strict';
+var knex = require('../db/knex.js');
 
 function newPlayerHandler (req, res, next) {
   var newPlayer = {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    color: req.body.color
+    color: req.body.color,
+    gender: req.body.gender
   };
 
 //check to see if email already exists in DB
+knex('players')
+.select()
+.where('email', newPlayer.email)
+.then(function(data){
+  res.send(data);
+})
 // if not, then insert with KNEX
 
 }
