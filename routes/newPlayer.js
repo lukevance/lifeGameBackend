21099e7167ac32c5d96fc642181db221000e8cfa
Matt Mobley <mattsmobley@gmail.com>
2016-02-25 07:54:23 -0700

    added the new player route

--- routes/newPlayer.js
+++ routes/newPlayer.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function newPlayerHandler (req, res, next) {
-  res.send('you made it!!!');
+  var newPlayer = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    color: req.body.color
+  };
+
+//check to see if email already exists in DB
+// if not, then insert with KNEX
 
 }
 
